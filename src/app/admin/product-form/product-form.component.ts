2d0860416e959a4fbcb93c800c707bda0b81730b
alfredo contreras <alfredolcontrerasl@gmail.com>
2019-04-12 15:38:05 -0400

    edit and delete button

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -11,7 +11,8 @@ import 'rxjs/add/operator/take';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
-  product;
+  product =  {};
+  id;
 
   constructor(
     private router: Router,
@@ -20,14 +21,24 @@ export class ProductFormComponent implements OnInit {
     private productService: ProductService) {
     this.categories$ = categoryService.getCategories();
 
-    let id = this.route.snapshot.paramMap.get('id');
-    if (id) this.productService.get(id).take(1).subscribe(p => this.product = p);
+    console.log(this.categories$.router);
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) this.productService.get(this.id).take(1).subscribe(p => this.product = p);
    }
    
 
    save(product) {
-     this.productService.create(product);
-     this.categories$.router.navigate(['/admin/products']);
+     if (this.id) this.productService.update(this.id, product);
+     else this.productService.create(product);
+     
+     return this.router.navigate(['/admin/products']);
+   }
+
+   delete() {
+      if(!confirm('are you sure you want to delete this product?')) return;
+
+      this.productService.delete(this.id);
+      this.router.navigate(['/admin/products']);
    }
 
   ngOnInit() {
